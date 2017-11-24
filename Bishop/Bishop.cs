59a693efe765fd7b0938ff3f44d8a40c9ec5bfa9
orlindraganov <orlindraganov@gmail.com>
2017-11-24 14:53:36 +0200

    Init
    Done tasks 1.FillTheMatrix & 7.Bishop

---
+++ Bishop/Bishop.cs
@@ -0,0 +1,135 @@
+﻿namespace Bishop
+{
+    using System;
+    using System.Linq;
+
+    class Bishop
+    {
+        static void Main()
+        {
+            int[] dims = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int[,] chessBoard = new int[dims[0], dims[1]];
+
+            for (int row = 0; row < chessBoard.GetLength(0); row++)
+            {
+                var thisRowFirstElement = chessBoard.GetLength(0) - row - 1;
+
+                for (int col = 0; col < chessBoard.GetLength(1); col++)
+                {
+                    chessBoard[row, col] = (thisRowFirstElement + col) * 3;
+                }
+            }
+
+            int[] position = { chessBoard.GetLength(0) - 1, 0 };
+
+            var numberOfMoves = int.Parse(Console.ReadLine());
+
+            long sum = 0;
+
+            for (int i = 0; i < numberOfMoves; i++)
+            {
+                string move = Console.ReadLine();
+
+                string direction = move.Substring(0, 2);
+                int numberOfSteps = int.Parse(move.Substring(3));
+
+                switch (direction)
+                {
+                    case "UR":
+                    case "RU":
+
+                        for (int j = 1; j < numberOfSteps; j++)
+                        {
+
+                            if ((position[0] > 0) && (position[1] < chessBoard.GetLength(1) - 1))
+                            {
+                                position[0]--;
+                                position[1]++;
+
+                                sum += chessBoard[position[0], position[1]];
+                                chessBoard[position[0], position[1]] = 0;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+                        break;
+
+                    case "UL":
+                    case "LU":
+
+                        for (int j = 1; j < numberOfSteps; j++)
+                        {
+
+                            if ((position[0] > 0) && (position[1] > 0))
+                            {
+                                position[0]--;
+                                position[1]--;
+
+                                sum += chessBoard[position[0], position[1]];
+                                chessBoard[position[0], position[1]] = 0;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+                        break;
+
+                    case "DR":
+                    case "RD":
+
+                        for (int j = 1; j < numberOfSteps; j++)
+                        {
+
+                            if ((position[0] < chessBoard.GetLength(0) - 1) && (position[1] < chessBoard.GetLength(1) - 1))
+                            {
+                                position[0]++;
+                                position[1]++;
+
+                                sum += chessBoard[position[0], position[1]];
+                                chessBoard[position[0], position[1]] = 0;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+                        break;
+
+                    case "DL":
+                    case "LD":
+
+                        for (int j = 1; j < numberOfSteps; j++)
+                        {
+
+                            if ((position[0] < chessBoard.GetLength(0) - 1) && (position[1] > 0))
+                            {
+                                position[0]++;
+                                position[1]--;
+
+                                sum += chessBoard[position[0], position[1]];
+                                chessBoard[position[0], position[1]] = 0;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ FillTheMatrix/FillTheMatrix.cs
@@ -0,0 +1,180 @@
+﻿namespace FillTheMatrix
+{
+    using System;
+
+    class FillTheMatrix
+    {
+        static void Main()
+        {
+            var size = int.Parse(Console.ReadLine());
+            var type = char.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[size, size];
+
+            var counter = 1;
+
+            switch (type)
+            {
+                case 'a':
+
+                    for (int col = 0; col < size; col++)
+                    {
+                        for (int row = 0; row < size; row++)
+                        {
+                            matrix[row, col] = counter;
+                            counter++;
+                        }
+                    }
+
+                    break;
+
+                case 'b':
+
+                    for (int col = 0; col < size; col++)
+                    {
+                        for (int i = 0; i < size; i++)
+                        {
+                            int row;
+
+                            if (col % 2 == 0)
+                            {
+                                row = i;
+                            }
+                            else
+                            {
+                                row = size - i - 1;
+                            }
+                            matrix[row, col] = counter;
+                            counter++;
+                        }
+                    }
+
+                    break;
+
+                case 'c':
+
+                    for (int i = 0; i < size * 2 - 1; i++)
+                    {
+                        int row;
+                        int col;
+                        if (i < size)
+                        {
+                            row = size - i - 1;
+                            col = 0;
+
+                            for (int j = 0; j <= i; j++)
+                            {
+                                matrix[row, col] = counter;
+                                counter++;
+
+                                row++;
+                                col++;
+                            }
+                        }
+                        else
+                        {
+                            row = 0;
+                            col = i - size + 1;
+
+                            for (int j = 0; j < 2 * size - i - 1; j++)
+                            {
+                                matrix[row, col] = counter;
+                                counter++;
+
+                                row++;
+                                col++;
+                            }
+                        }
+                    }
+
+                    break;
+
+                case 'd':
+
+                    var direction = "down";
+                    int[] position = { 0, 0 };
+
+                    for (int i = 0; i < size * size; i++)
+                    {
+                        matrix[position[0], position[1]] = counter;
+                        counter++;
+
+                        switch (direction)
+                        {
+                            case "down":
+
+                                if ((position[0] + 1 < size) && (matrix[position[0] + 1, position[1]] == 0))
+                                {
+                                    position[0]++;
+                                }
+                                else
+                                {
+                                    direction = "right";
+                                    position[1]++;
+                                }
+
+                                break;
+
+                            case "right":
+
+                                if ((position[1] + 1 < size) && (matrix[position[0], position[1] + 1] == 0))
+                                {
+                                    position[1]++;
+                                }
+                                else
+                                {
+                                    direction = "up";
+                                    position[0]--;
+                                }
+
+                                break;
+
+                            case "up":
+                                if ((position[0] > 0) && (matrix[position[0] - 1, position[1]] == 0))
+                                {
+                                    position[0]--;
+                                }
+                                else
+                                {
+                                    direction = "left";
+                                    position[1]--;
+                                }
+
+                                break;
+
+                            case "left":
+
+                                if ((position[1] > 0) && (matrix[position[0], position[1] - 1] == 0))
+                                {
+                                    position[1]--;
+                                }
+                                else
+                                {
+                                    direction = "down";
+                                    position[0]++;
+                                }
+
+                                break;
+
+                            default:
+                                break;
+                        }
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size - 1; col++)
+                {
+                    Console.Write(matrix[row, col]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine(matrix[row, size - 1]);
+            }
+        }
+    }
+}
